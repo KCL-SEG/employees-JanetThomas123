d87adb7c1532f77c511cf55f8cd90a13c500fbdb
JanetThomas123 <115407383+JanetThomas123@users.noreply.github.com>
2022-12-08 22:33:04 +0000

    Update employee.py

--- employee.py
+++ employee.py
@@ -2,14 +2,40 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name):
-        self.name = name
+def __init__(self, name, salary = 0, pay_rate = 0, contract_hours = 0, commission_contracts = 0, bonus_commission=0, commision_rate =0):
+    self.name = name
+    self.pay_rate = pay_rate
+    self.salary = salary
+    self.contract_hours = contract_hours
+    self.commission_contracts = commission_contracts
+    self.bonus_commission = bonus_commission
+    self.commision_rate = commision_rate
 
-    def get_pay(self):
-        pass
+def get_pay(self):
+    pay = 0
+        if self.salary:
+            pay += self.salary
+        if self.contract_hours and self.pay_rate:
+            pay += self.contract_hours * self.pay_rate
+        if self.commission_contracts and self.commision_rate:
+            pay += self.commission_contracts * self.commision_rate
+        if self.bonus_commission:
+            pay += self.bonus_commission
+return pay
 
-    def __str__(self):
-        return self.name
+def __str__(self):
+    s = f'{self.name} works on '
+        if self.salary:
+            s += f'a monthly salary of {self.salary} '
+        if self.contract_hours and self.pay_rate:
+            s += f'a contract of {self.contract_hours} hours at {self.pay_rate}/hour '
+        if self.commission_contracts and self.commision_rate:
+            s += f'and receives a commission for {self.commission_contracts} contract(s) at {self.commision_rate}/contract. '
+        if self.bonus_commission:
+            s += f'and receives a bonus commission of {self.bonus_commission}.'
+            s += f' Their total pay is {self.get_pay()}.'
+    return s
+
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
